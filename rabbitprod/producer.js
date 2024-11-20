ba5b0ade9a0970defa915d453be31c77de50c4ba
jardianagalvao <jardianagalvao@gmail.com>
2024-11-20 01:46:42 -0300

    atualizado com atividade

--- rabbitprod/producer.js
+++ rabbitprod/producer.js
@@ -1,31 +1,28 @@
 const amqp = require('amqplib');
-const doenv = require('dotenv').config();
+const dotenv = require('dotenv').config();
 
 async function sendMessage() {
-    let queue = 'hello';
-    let message = 'Hello World!';
-
+    let queue = 'transactions'; // Fila correta
+    let amountToDeduct = 50; // Quantia a ser deduzida
     let url = process.env.AMQP;
-    
-    try{
+
+    try {
         const connection = await amqp.connect(url);
         const channel = await connection.createChannel();
-        await channel.assertQueue(queue, {durable : false});
+        await channel.assertQueue(queue, { durable: false });
 
+        // Envia a quantia a ser deduzida
+        const message = JSON.stringify({ amount: amountToDeduct });
         channel.sendToQueue(queue, Buffer.from(message));
         console.log(`Mensagem enviada: ${message}`);
 
-        setTimeout(()=>{
+        setTimeout(() => {
             connection.close();
             process.exit(0);
         }, 500);
-    }catch(error){
+    } catch (error) {
         console.error(error);
     }
-    
-    
 }
 
 sendMessage();
-
-// instalar o reptmqueue
